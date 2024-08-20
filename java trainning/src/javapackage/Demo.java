9b8b8a0dc6adc665f5d6a75e785f338fc7d29cb2
pravinkumar124 <pravinkumar1242000@gmail.com>
2024-08-20 11:35:10 +0530

    commited java program

---
+++ java trainning/src/javapackage/Demo.java
@@ -0,0 +1,19 @@
+package javapackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=5;
+		int sub = a-b;
+		
+		System.out.println("suberact is "+sub);
+		
+		
+		
+		
+
+	}
+
+}
